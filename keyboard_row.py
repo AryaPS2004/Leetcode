4a57f2e00cfc214365e47d9283a8542fc8dd23bd
AryaPS2004 <111893091+AryaPS2004@users.noreply.github.com>
2026-02-19 00:28:14 +0530

    Create keyboard_row.py

---
+++ keyboard_row.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findWords(self, words):
+        row1 = set("qwertyuiop")
+        row2 = set("asdfghjkl")
+        row3 = set("zxcvbnm")
+        res=[]
+        for i in words:
+            w=set(i.lower())
+            if w<=row1 or w<=row2 or w<=row3:
+                res.append(i)
+        return res
