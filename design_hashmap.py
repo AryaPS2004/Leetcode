316635c742e4348b7ef4b2a3896d9015f0974f5e
AryaPS2004 <111893091+AryaPS2004@users.noreply.github.com>
2026-02-22 19:13:14 +0530

    Create design_hashmap.py

---
+++ design_hashmap.py
@@ -0,0 +1,17 @@
+class MyHashMap(object):
+
+    def __init__(self):
+        self.data= [-1]*1000001
+        
+
+    def put(self, key, value):
+        self.data[key]=value
+        
+
+    def get(self, key):
+        return self.data[key]
+        
+
+    def remove(self, key):
+        self.data[key]=-1
+        
